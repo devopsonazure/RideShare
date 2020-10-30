10dc73d8a43fefe84f01ab1a1120fd1eeb895406
Yingting Huang <ythuang@microsoft.com>
2020-10-30 14:31:29 +0800

    Add settings.js

---
+++ web/serverless-microservices-web/public/js/settings.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '8bce8556-8fa1-4dce-9602-3bc1c60a6a7c';
+window.authAuthority = 'https://rideshareaadb2c.b2clogin.com/tfp/rideshareaadb2c.onmicrosoft.com/b2c_1_default_signin/v2.0';
+window.authScopes = ['https://rideshareaadb2c.onmicrosoft.com/8bce8556-8fa1-4dce-9602-3bc1c60a6a7c/rideshare'];
+window.authEnabled = false;
+
+// API endpoints
+window.apiKey = '8fce1aff7dc54be09876cc2905c2c225';
+window.apiBaseUrl = 'https://rideshare.azure-api.net';
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+window.signalrInfoUrl = 'https://ridesharetrip.azurewebsites.net/api/GetSignalRInfo';
